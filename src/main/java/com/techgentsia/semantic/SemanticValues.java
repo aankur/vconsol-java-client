c3d0061d8bcec4a64df02e4a675f25ba8908d760
Ankur Deep Jaiswal <ankur@techgentsia.com>
2024-12-06 09:47:59 +0530

    Add More Semantic Values

--- src/main/java/com/techgentsia/semantic/SemanticValues.java
+++ src/main/java/com/techgentsia/semantic/SemanticValues.java
@@ -2,4 +2,10 @@ package com.techgentsia.semantic;
 
 public class SemanticValues {
   public static String USER_TYPE_PERMANENT = "Permanent";
+  public static String USER_TYPE_TEMPORARY = "Temporary";
+  public static String USER_TYPE_TRIAL = "Trial";
+
+
+  public static String USER_GROUP_CENTRAL = "Central";
+  public static String USER_GROUP_STATE = "State";
 }
